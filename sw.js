cb4606531921df3e857b4df61fc5546b1c8ed639
LeCreatre <gamebase070707@gmail.com>
2025-10-15 14:44:08 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'teammy-cache-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
